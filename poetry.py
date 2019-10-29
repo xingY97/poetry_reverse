fabdf04b9a7a2eab8ffb547bb34ef19be5853985
Xing_Ye <YOUR_PERSONAL_EMAIL@example.com>
2019-10-29 11:15:22 -0700

    starter code

---
+++ poetry.py
@@ -0,0 +1,26 @@
+poem = """If you are a dreamer, come in,
+If you are a dreamer, a wisher, a liar,
+A hope-er, a pray-er, a magic bean buyer…
+If you’re a pretender, come sit by my fire
+For we have some flax-golden tales to spin.
+Come in!
+Come in!"""
+
+def line_printed_backwards():
+    """This function takes in a list of strings containing the 
+    lines of your poem as arguments and will print the poem lines out in 
+    reverse with the line numbers reversed"""
+    pass
+
+def lines_printed_random():
+    """randomly select lines from a list of strings and print them out in random order"""
+    pass
+
+def my_custom_function():
+    """does somthing of my choosing 
+    TODO: comment"""
+
+#testing code
+#line_printed_backwards()
+#lines_printed_random()
+#my_custom_function()
