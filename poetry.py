9dc2beea47201abba28f3c21b97b7a83234f7fe5
Xing_Ye <YOUR_PERSONAL_EMAIL@example.com>
2019-10-29 14:56:58 -0700

    finished line_printed_backwards function, printed poem in reverse pattern with its line number

--- poetry.py
+++ poetry.py
@@ -6,12 +6,22 @@ For we have some flax-golden tales to spin.
 Come in!
 Come in!"""
 
-def line_printed_backwards():
+def line_printed_backwards(lines_list):
     """This function takes in a list of strings containing the 
     lines of your poem as arguments and will print the poem lines out in 
     reverse with the line numbers reversed"""
-    pass
+    #TODO:reverse the list
+    
+    lines_list.reverse()
+    #print(lines_list)
+    #TODO: use loop to print out items in list
+    list_num = len(lines_list)
+    
+    for i in range(len(lines_list)):
+        print(str(list_num) +" " + lines_list[i])
+        list_num -= 1
 
+    
 def lines_printed_random():
     """randomly select lines from a list of strings and print them out in random order"""
     pass
@@ -21,6 +31,8 @@ def my_custom_function():
     TODO: comment"""
 
 #testing code
-#line_printed_backwards()
+#TODO:Get poem string into list of lines
+lines_list = poem.split("\n")
+line_printed_backwards(lines_list)
 #lines_printed_random()
 #my_custom_function()
