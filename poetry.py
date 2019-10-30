686c0ab0a6567c9fa4ea8da03dac734b2323f56d
Xing_Ye <YOUR_PERSONAL_EMAIL@example.com>
2019-10-30 13:43:33 -0700

    finished

--- poetry.py
+++ poetry.py
@@ -5,7 +5,7 @@ If you are a dreamer, a wisher, a liar,
 A hope-er, a pray-er, a magic bean buyer…
 If you’re a pretender, come sit by my fire
 For we have some flax-golden tales to spin.
-Come in!
+Come in--
 Come in!"""
 
 def line_printed_backwards(lines_list):
@@ -15,25 +15,37 @@ def line_printed_backwards(lines_list):
     #TODO:reverse the list
     
     lines_list.reverse()
-    #print(lines_list)
+
     #TODO: use loop to print out items in list
+
     list_num = len(lines_list)
-    
     for i in range(len(lines_list)):
         print(str(list_num) +" " + lines_list[i])
         list_num -= 1
 
 def lines_printed_random():
     """randomly select lines from a list of strings and print them out in random order"""
-    print(random.choice(lines_list))
+    print( "\033[1;33;40mrandom line: " + random.choice(lines_list))
 
 def my_custom_function():
-    """does somthing of my choosing 
-    TODO: comment"""
+    """does something of my choosing """
+    
+    list_num = len(lines_list)
+    print( "\ncustom function print all even number lines.")
+    
+    for i in range(len(lines_list)):
+        if i % 2 == 0:
+            print(str(i) + " " + lines_list[i])
+            
+
+    
+    
+
+    
 
 #testing code
 #TODO:Get poem string into list of lines
 lines_list = poem.split("\n")
 line_printed_backwards(lines_list)
 lines_printed_random()
-#my_custom_function()
+my_custom_function()
